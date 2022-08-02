4b87a61b59f1e8681a4744cbc64c4342e9d2b0e0
Яхина Наталья Андреевна <  @vvrz.local>
2022-08-02 10:57:23 +0300

    0208

---
+++ algoritmEvklida/Program.cs
@@ -0,0 +1,27 @@
+﻿// НОД (Алгоритм Евклида)
+// НОД (14, 21) = 7 // наибольший общий делитель 
+
+// int n = 140;
+// int m = 175;
+// while (n != m)
+// { 
+//     if (n > m) n = n - m; 
+//     else m = m - n; 
+//     Console.WriteLine(n); 
+//     Console.WriteLine(m); 
+//     Console.WriteLine();
+// }
+
+
+// НОК
+int n = 140;
+int m = 175;
+int count = n * m;
+while (n != m)
+{
+    if (n > m)
+        n = n - m;
+    else
+        m = m - n;
+}
+Console.WriteLine(count / n);
